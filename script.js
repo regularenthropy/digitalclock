fa5d70a00d3d0b039107f4ea5013b1fe2b05b56a
regularenthropy <roottotalhax@gmail.com>
2022-08-23 18:15:05 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function display_time() {
+    let time = new Date();
+    let hrs = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+
+    document.getElementById('hours').innerHTML = hrs;
+    if (min < 10){
+        document.getElementById('minutes').innerHTML = "0" + min;
+    } else {
+        document.getElementById('minutes').innerHTML = min;
+    }
+    if (sec < 10) {
+        document.getElementById('seconds').innerHTML = "0" + sec;
+    } else {
+        document.getElementById('seconds').innerHTML = sec;
+    }
+}
+
+setInterval(display_time, 10);
